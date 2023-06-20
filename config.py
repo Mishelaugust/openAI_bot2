a7f13686d4df2e0e86229a92d28d8eabf33f1626
Mishelaugust <117676153+Mishelaugust@users.noreply.github.com>
2023-06-20 22:54:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-tg_token = '5908362270:AAGssX2MsxPllkHSZP4bsq2Gt30Impmr9wo'
-api_token = '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'
+tg_token = 'token'
+api_token = 'token'
 url = 'https://api.openai.com/v1/images/generations'
  
